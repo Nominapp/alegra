c1c09942ee98d17e16812bd640e23a275fd20598
Miguel Ferrer <miguef7@gmail.com>
2020-02-25 10:12:31 -0500

    add company endpoint wrapper

--- lib/alegra/company.rb
+++ lib/alegra/company.rb
@@ -1,48 +1,12 @@
 module Alegra
-  class Company
-    attr_reader :client
-
-    def initialize(client)
-      @client = client
+  class Company < Alegra::Record
+    def find(options = { format: :formated })
+      client.get('company', {}, options)
     end
 
-    # Returns company
-    # @param params [ Hash ]
-    #   - start [ Integer ]
-    #   - limit [ Integer ]
-    #   - order_direction [ String ]
-    #   - order_field [ string ]
-    #   - query [ String ]
-    #   - type [ Integer ]
-    #   - metadata [ Boolean ]
-    #   - name [ String ]
-    #   - identification [ String ]
-    # @return [ Array ]
-    def find(params = {})
-      client.get('company', params)
-    end
-
-    # @param id [ Integer ]
-    # @param params [ Hash ]
-    #   - name [ String ]
-    #   - identification [ String ]
-    #   - email [ String ]
-    #   - phone_primary [ String ]
-    #   - phone_secondary [ String ]
-    #   - fax [ String ]
-    #   - mobile [ String ]
-    #   - observations [ String ]
-    #   - ignore_repeated [ Boolean ]
-    #   - price_list [ Hash ]
-    #   - seller [ Hash ]
-    #   - term [ Hash ]
-    #   - type [ Array ]
-    #   - address [ Hash ]
-    #   - internal_contacts [ Array ]
-    # @return [ Hash ]
-    def update(id, params)
+    def update(params)
       sanitize_params = params.deep_camel_case_lower_keys
-      client.put("contacts/#{id}", sanitize_params)
+      client.put('company', sanitize_params)
     end
   end
 end
